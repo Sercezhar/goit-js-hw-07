7fe6a187de9ddaa5ba6c924065f8813b8141ae6f
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-01-21 21:28:14 +0200

    task-1: done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const images = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+          class="gallery__image"
+          src="${image.preview}"
+          data-source="${image.original}"
+          alt="${image.description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+galleryRef.innerHTML = images;
+
+const makeImageBigger = (event) => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"  alt="${event.target.alt}"/>`
+  );
+  instance.show();
+
+  const closeImgByEsc = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  };
+  window.addEventListener("keydown", closeImgByEsc);
+};
+
+galleryRef.addEventListener("click", makeImageBigger);
